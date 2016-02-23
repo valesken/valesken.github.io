c37384333df8a3a308bd1902087110ec41f27caf
valesken <jeff.valesken@gmail.com>
2016-02-23 10:20:16 -0800

    Adding animation to subtitle

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
+var phrases = ['Software Engineer', 'Traveller', 'Mobile Developer', 'Book Lover', 'Grad Student'];
+var position = 0;
+
+function advancePosition() {
+    if (position === (phrases.length - 1)) {
+        position = 0;
+    } else {
+        position += 1;
+    }
+};
+
+function changeText() {
+    $('.option').removeClass('fadeIn');
+    $('.option').addClass('fadeOut');
+    advancePosition();
+    setTimeout(function() {
+        $('.option').text(phrases[position]);
+        $('.option').removeClass('fadeOut');
+        $('.option').addClass('fadeIn');
+    }, 1500);
+}
+
+function changeInitialText() {
+    changeText();
+    setInterval(changeText, 5000);
+}
+
 $(document).ready(function() {
     $('#main').css({
         'margin-left': ($('.container').width() - $('#main').outerWidth()) / 2,
         'margin-top': ($(window).height() - $('#main').outerHeight()) / 2
     });
-})
+    setTimeout(changeInitialText, 3000);
+});
